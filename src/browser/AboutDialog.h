3a1e8a92a08d82adfd6c08303d445b84ec4ffdd2
Jake Petroules <jake.petroules@petroules.com>
2012-11-21 08:59:03 -0500

    Whitespace fixes.

--- src/browser/AboutDialog.h
+++ src/browser/AboutDialog.h
@@ -46,18 +46,19 @@
 
 #include <QDialog>
 
-namespace Ui {
-class AboutDialog;
+namespace Ui
+{
+    class AboutDialog;
 }
 
 class AboutDialog : public QDialog
 {
     Q_OBJECT
-    
+
 public:
     explicit AboutDialog(QWidget *parent = 0);
     ~AboutDialog();
-    
+
 private:
     Ui::AboutDialog *ui;
 };
