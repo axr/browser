1fb23b460002d381aacb2c407e97f5252e2630a1
Jake Petroules <jake.petroules@petroules.com>
2012-11-25 20:26:08 -0500

    Sync with core's make.sh if we want to add more configurations later.

--- .travis/make.sh
+++ .travis/make.sh
@@ -4,6 +4,11 @@ set -e
 # Build all variants
 cd ..
 
-cmake --build build --config Release --target all
-cmake --build build --config Release --target distribution
-cmake --build build --config Release --target package_source
+build-targets()
+{
+    cmake --build $1 --config Release --target all
+    cmake --build $1 --config Release --target distribution
+    cmake --build $1 --config Release --target package_source
+}
+
+build-targets build
